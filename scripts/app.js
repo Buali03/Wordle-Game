ed35bfce19cd2078d7b3122e8b8bcabb655bb55b
Ali Buali <alibuali3660@gmail.com>
2025-07-13 10:50:41 +0300

    adding the keyboard using js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,14 @@
+const keyboardElem = document.querySelector(".keyboard");
+for (let i = 65; i <= 90; i++) {
+  const button = document.createElement("button");
+  button.innerText = String.fromCharCode(i);
+  keyboardElem.appendChild(button);
+  button.addEventListener("click", function (event) {
+    event.target.innerText = String.fromCharCode(i);
+    cuurentLetter = event.target.innerText;
+    init();
+  });
+}
+
 function init() {}
+init();
