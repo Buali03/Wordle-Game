f16c1629a2f90634860ad7607a45934a50e21402
Ali Buali <alibuali3660@gmail.com>
2025-07-13 11:52:15 +0300

    Adding functionality to the keyboard keys and delete key

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,23 @@
+// Creating the keyboard keys
 const keyboardElem = document.querySelector(".keyboard");
+const letterElem = document.querySelector(".letters");
+const guessOne = [];
+const currentGuess = [];
+
 for (let i = 65; i <= 90; i++) {
   const button = document.createElement("button");
   button.innerText = String.fromCharCode(i);
+  button.classList.add("key");
   keyboardElem.appendChild(button);
+
   button.addEventListener("click", function (event) {
     event.target.innerText = String.fromCharCode(i);
-    cuurentLetter = event.target.innerText;
-    init();
+    currentLetter = event.target.innerText;
+    if (currentGuess.length < 5) {
+      currentGuess.push(currentLetter);
+      letterElem.textContent = currentLetter;
+      console.log(currentGuess);
+    }
   });
 }
 const deleteButton = document.createElement("button");
@@ -14,9 +25,23 @@ deleteButton.innerText = "DELETE";
 deleteButton.classList.add("del");
 keyboardElem.appendChild(deleteButton);
 
+deleteButton.addEventListener("click", function () {
+  currentGuess.pop();
+  console.log(currentGuess);
+  console.log("delete clicked");
+});
+
 const enterButton = document.createElement("button");
 enterButton.innerText = "ENTER";
 enterButton.classList.add("ent");
 keyboardElem.appendChild(enterButton);
-function init() {}
+
+// enterButton.addEventListener("click", function () {
+//   if (currentGuess.length === 5) {
+//   }
+// });
+
+function init() {
+  //   const guessElem = document.querySelector();
+}
 init();
