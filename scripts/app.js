cbb1e8b3e8cd41c5eb83043cc27627d8f1786cad
Ali Buali <alibuali3660@gmail.com>
2025-07-13 16:15:52 +0300

    Changed to querySelectorAll for the guessing letters

--- scripts/app.js
+++ scripts/app.js
@@ -1,58 +1,81 @@
-// Creating the keyboard keys
+// constants
 const keyboardElem = document.querySelector(".keyboard");
-const wordOne1Elem = document.querySelector("#one");
-const wordOne2Elem = document.querySelector("#two");
-const wordOne3Elem = document.querySelector("#three");
-const wordOne4Elem = document.querySelector("#four");
-const wordOne5Elem = document.querySelector("#five");
+const guessingWordElem = document.querySelectorAll(".letters");
+let position = 0;
+let count = 0;
+let count2 = 0;
 const guessOne = [];
 const currentGuess = [];
+const chosenWord = ["A", "D", "M", "I", "N"];
 
+// creating keyboard buttons
 for (let i = 65; i <= 90; i++) {
   const button = document.createElement("button");
   button.innerText = String.fromCharCode(i);
   button.classList.add("key");
   keyboardElem.appendChild(button);
+
+  // letter buttons event listener
   button.addEventListener("click", function (event) {
     currentLetter = String.fromCharCode(i);
     if (currentGuess.length < 5) {
       currentGuess.push(currentLetter);
       updateLetters();
-      console.log(currentGuess);
+      console.log(currentGuess); // delete later
     }
   });
 }
+// creating delete button
 const deleteButton = document.createElement("button");
 deleteButton.innerText = "DELETE";
 deleteButton.classList.add("del");
 keyboardElem.appendChild(deleteButton);
 
+// delete button event listener
 deleteButton.addEventListener("click", function () {
-  currentGuess.pop();
-  updateLetters();
-  console.log(currentGuess);
-  console.log("delete clicked");
+  if (currentGuess.length > 0) {
+    deleteLetter();
+    console.log(currentGuess); // delete later
+    console.log("delete clicked"); // delete later
+  }
 });
 
+// creating enter button
 const enterButton = document.createElement("button");
 enterButton.innerText = "ENTER";
 enterButton.classList.add("ent");
 keyboardElem.appendChild(enterButton);
 
-function render() {}
-// enterButton.addEventListener("click", function () {
-//   if (currentGuess.length === 5) {
-//   }
-// });
+// enter button event listner
+
+enterButton.addEventListener("click", function () {
+  if (currentGuess.length === 5) {
+    check();
+    position += 5;
+    count = 0;
+  }
+});
+
+// update display function
 function updateLetters() {
-  wordOne1Elem.textContent = currentGuess[0];
-  wordOne2Elem.textContent = currentGuess[1];
-  wordOne3Elem.textContent = currentGuess[2];
-  wordOne4Elem.textContent = currentGuess[3];
-  wordOne5Elem.textContent = currentGuess[4];
+  guessingWordElem[count2].textContent = currentGuess[count];
+  count++;
+  count2++;
+  console.log(currentGuess); // delete later
 }
-function init() {
-  //   const guessElem = document.querySelector();
-  render();
+
+// delete letter function
+function deleteLetter() {
+  currentGuess.pop();
+  count--;
+  count2--;
 }
+// checking if the guess is correct function
+function check() {
+  for (let i = 0; i < currentGuess.length; i++)
+    if (currentGuess[i] === chosenWord[i]) {
+      guessingWordElem[i].style.backgroundColor = "#1bb152";
+    }
+}
+function init() {}
 init();
