c1071495eb83dda6f4718e1bc6ed6570e5eb16c4
Ali Buali <alibuali3660@gmail.com>
2025-07-14 13:31:12 +0300

    Adding theme switch (light mode/dark mode)

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,11 @@
 // constants
 const keyboardElem = document.querySelector(".keyboard");
 const guessingWordElem = document.querySelectorAll(".letters");
-let position = -1;
+const themeSwitchElem = document.querySelector(".theme-switch");
+const bodyElem = document.querySelector("body");
 let arrayCount = 0;
 let displayCount = 0;
+let position = 0;
 let guessCount;
 const guessOne = [];
 const currentGuess = [];
@@ -20,8 +22,7 @@ for (let i = 65; i <= 90; i++) {
   button.addEventListener("click", function (event) {
     currentLetter = String.fromCharCode(i);
     if (currentGuess.length < 5) {
-      currentGuess.push(currentLetter);
-      updateLetters();
+      addLetters();
       console.log(currentGuess); // delete later
     }
   });
@@ -58,7 +59,8 @@ enterButton.addEventListener("click", function () {
 });
 
 // update display function
-function updateLetters() {
+function addLetters() {
+  currentGuess.push(currentLetter);
   guessingWordElem[displayCount].textContent = currentGuess[arrayCount];
   arrayCount++;
   displayCount++;
@@ -71,12 +73,21 @@ function deleteLetter() {
   currentGuess.pop();
   arrayCount--;
   displayCount--;
+  position--;
   console.log(position);
   guessingWordElem[position].textContent = "";
-  position--;
 }
 
 // checking if the guess is correct function
 function check() {}
+
+// added page theme switch (light mode/dark mode)
+themeSwitchElem.addEventListener("click", function (event) {
+  if (event.target.checked === true) {
+    bodyElem.classList.add("darkmode");
+  } else {
+    bodyElem.classList.remove("darkmode");
+  }
+});
 function init() {}
 init();
