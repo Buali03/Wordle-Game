c5b2b53433e66648cfea02d76fa8ebedc0b327e2
Ali Buali <alibuali3660@gmail.com>
2025-07-14 16:19:56 +0300

    Adding check guess function

--- scripts/app.js
+++ scripts/app.js
@@ -5,11 +5,10 @@ const themeSwitchElem = document.querySelector(".theme-switch");
 const bodyElem = document.querySelector("body");
 let arrayCount = 0;
 let displayCount = 0;
-let position = 0;
-let guessCount;
-const guessOne = [];
+let wordCount = 0;
+const checkingDupArr = [];
 const currentGuess = [];
-const chosenWord = ["A", "D", "M", "I", "N"];
+const chosenWord = ["A", "R", "R", "O", "W"];
 
 // creating keyboard buttons
 for (let i = 65; i <= 90; i++) {
@@ -23,7 +22,6 @@ for (let i = 65; i <= 90; i++) {
     currentLetter = String.fromCharCode(i);
     if (currentGuess.length < 5) {
       addLetters();
-      console.log(currentGuess); // delete later
     }
   });
 }
@@ -38,8 +36,6 @@ keyboardElem.appendChild(deleteButton);
 deleteButton.addEventListener("click", function () {
   if (currentGuess.length > 0) {
     deleteLetter();
-    console.log(currentGuess); // delete later
-    console.log("delete clicked"); // delete later
   }
 });
 
@@ -52,7 +48,7 @@ keyboardElem.appendChild(enterButton);
 // enter button event listner
 enterButton.addEventListener("click", function () {
   if (currentGuess.length === 5) {
-    check();
+    checkGuess();
     currentGuess.length = 0;
     arrayCount = 0;
   }
@@ -64,8 +60,6 @@ function addLetters() {
   guessingWordElem[displayCount].textContent = currentGuess[arrayCount];
   arrayCount++;
   displayCount++;
-  position++;
-  console.log(currentGuess); // delete later
 }
 
 // delete letter function
@@ -73,13 +67,50 @@ function deleteLetter() {
   currentGuess.pop();
   arrayCount--;
   displayCount--;
-  position--;
-  console.log(position);
-  guessingWordElem[position].textContent = "";
+  guessingWordElem[displayCount].textContent = "";
 }
 
 // checking if the guess is correct function
-function check() {}
+function checkGuess() {
+  // counting the amount of times each letter is repeated in the chosen word
+  const counts = {};
+  chosenWord.forEach(function (l) {
+    if (counts[l] === undefined) {
+      counts[l] = 1;
+    } else {
+      counts[l]++;
+    }
+  });
+
+  const result = Array(5).fill("gray");
+  const displayOrder = displayCount - 5;
+
+  // checks if guess is green (right letter, right position)
+  for (let i = 0; i < 5; i++) {
+    if (currentGuess[i] === chosenWord[i]) {
+      result[i] = "green";
+      counts[currentGuess[i]]--;
+    }
+  }
+
+  // checks if guess is orange (right letter, wrong position)
+  for (let i = 0; i < 5; i++) {
+    if (counts[currentGuess[i]] > 0 && result[i] === "gray") {
+      result[i] = "orange";
+      counts[currentGuess[i]]--;
+    }
+  }
+
+  for (let i = 0; i < 5; i++) {
+    if (result[i] === "green") {
+      guessingWordElem[displayOrder + i].style.backgroundColor = "#6aaa64";
+    } else if (result[i] === "orange") {
+      guessingWordElem[displayOrder + i].style.backgroundColor = "#c9b458";
+    } else guessingWordElem[displayOrder + i].style.backgroundColor = "#787c7e";
+  }
+
+  wordCount++;
+}
 
 // added page theme switch (light mode/dark mode)
 themeSwitchElem.addEventListener("click", function (event) {
